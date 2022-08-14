becfc6645c565d636b0cfce6f20efbe1878e57cc
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-08-14 19:07:36 -0400

    Init firebase

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:quizapp/routes.dart';
 
 void main() {
@@ -24,7 +25,9 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   /// The future is part of the state of our widget. We should not call `initializeApp`
   /// directly inside [build].
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   @override
   Widget build(BuildContext context) {
