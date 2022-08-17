1ad4c6119da41f454f97b371d54d2a9ba608282e
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-08-17 18:16:15 -0400

    Add anon login and signout capability

--- quizapp/lib/profile/profile.dart
+++ quizapp/lib/profile/profile.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quizapp/services/auth.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: ElevatedButton(
+        child: const Text('Signout'),
+        onPressed: () async {
+          await AuthService().signOut();
+          Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
+        },
+      ),
+    );
   }
 }
