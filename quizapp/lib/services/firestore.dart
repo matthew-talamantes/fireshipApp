d0c1083865f35d52cef0e9d0bd7844cbf7fe4873
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-09-21 15:14:59 -0400

    Complete firestore service

--- quizapp/lib/services/firestore.dart
+++ quizapp/lib/services/firestore.dart
@@ -22,4 +22,31 @@ class FirestoreService {
     var snapshot = await ref.get();
     return Quiz.fromJson(snapshot.data() ?? {});
   }
+
+  // Listens to current user's report document in Firestore
+  Stream<Report> streamReport() {
+    return AuthService().userStream.switchMap((user) {
+      if (user != null) {
+        var ref = _db.collection('reports').doc(user.uid);
+        return ref.snapshots().map((doc) => Report.fromJson(doc.data()!));
+      } else {
+        return Stream.fromIterable([Report()]);
+      }
+    });
+
+    // Updates the current user's report document after completing quiz
+    Future<void> updateUserReport(Quiz quiz) {
+      var user = AuthService().user!;
+      var ref = _db.collection('reports').doc(user.uid);
+
+      var data = {
+        'total': FieldValue.increment(1),
+        'topics': {
+          quiz.topic: FieldValue.arrayUnion([quiz.id])
+        }
+      };
+
+      return ref.set(data, SetOptions(merge: true));
+    }
+  }
 }
