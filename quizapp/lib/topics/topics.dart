3afb74bf2b2b489169821f7ebad220ee306c81dd
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-09-21 19:31:36 -0400

    Add topics page, loding widget and error widget

--- quizapp/lib/topics/topics.dart
+++ quizapp/lib/topics/topics.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quizapp/services/firestore.dart';
+import 'package:quizapp/services/models.dart';
 import 'package:quizapp/shared/bottom_nav.dart';
+import 'package:quizapp/shared/error.dart';
+import 'package:quizapp/shared/loading.dart';
 
 class TopicsScreen extends StatelessWidget {
   const TopicsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: BottomNavBar(),
-    );
+    return FutureBuilder<List<Topic>>(
+        future: FirestoreService().getTopics(),
+        builder: ((context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const LoadingScreen();
+          } else if (snapshot.hasError) {
+            return Center(
+              child: ErrorMessage(message: snapshot.error.toString()),
+            );
+          } else if (snapshot.hasData) {
+            var topics = snapshot.data!;
+
+            return Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.deepPurple,
+                title: const Text('Topics'),
+              ),
+              body: GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20.0),
+                crossAxisSpacing: 10.0,
+                crossAxisCount: 2,
+                children: topics.map((topic) => Text(topic.title)).toList(),
+              ),
+              bottomNavigationBar: const BottomNavBar(),
+            );
+          } else {
+            return const Text('No topics found in Firestore. Check database');
+          }
+        }));
   }
 }
