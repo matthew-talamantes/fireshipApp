e27fd881555969d392c3ed3ce2bbdd60eee277c3
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-11-28 17:06:46 -0500

    Continue building out topics

--- quizapp/lib/topics/topics.dart
+++ quizapp/lib/topics/topics.dart
@@ -5,6 +5,7 @@ import 'package:quizapp/services/models.dart';
 import 'package:quizapp/shared/bottom_nav.dart';
 import 'package:quizapp/shared/error.dart';
 import 'package:quizapp/shared/loading.dart';
+import 'package:quizapp/topics/topic_item.dart';
 
 class TopicsScreen extends StatelessWidget {
   const TopicsScreen({Key? key}) : super(key: key);
@@ -33,7 +34,8 @@ class TopicsScreen extends StatelessWidget {
                 padding: const EdgeInsets.all(20.0),
                 crossAxisSpacing: 10.0,
                 crossAxisCount: 2,
-                children: topics.map((topic) => Text(topic.title)).toList(),
+                children:
+                    topics.map((topic) => TopicItem(topic: topic)).toList(),
               ),
               bottomNavigationBar: const BottomNavBar(),
             );
