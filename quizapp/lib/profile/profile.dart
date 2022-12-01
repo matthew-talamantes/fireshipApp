bd4a817e7be5e3586ac5b3c63fdf28dbc47ea227
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-11-30 21:32:05 -0500

    Build out profile page

--- quizapp/lib/profile/profile.dart
+++ quizapp/lib/profile/profile.dart
@@ -1,23 +1,86 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:quizapp/services/auth.dart';
+import 'package:quizapp/services/models.dart';
+import 'package:quizapp/shared/error.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: ElevatedButton(
-        child: const Text('Signout'),
-        onPressed: () async {
-          await AuthService().signOut();
-          Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
-        },
-      ),
+    var report = Provider.of<Report>(context);
+    var user = AuthService().user;
+
+    if (user != null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Profile'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset('assets/user.png', height: 200, width: 200),
+              QuizScore(report: report),
+              ElevatedButton(
+                child: const Text('Signout'),
+                onPressed: () async {
+                  await AuthService().signOut();
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil('/', (route) => false);
+                },
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Profile'),
+        ),
+        body: const Center(
+          child: ErrorMessage(message: 'How did you get here?'),
+        ),
+      );
+    }
+  }
+}
+
+class QuizScore extends StatelessWidget {
+  const QuizScore({
+    Key? key,
+    required this.report,
+  }) : super(key: key);
+
+  final Report report;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          report.total.toString(),
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            color: Color(0xFFD6D6D6),
+            fontSize: 48,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const Text(
+          'Quizzes Completed',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      ],
     );
   }
 }
