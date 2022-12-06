827fb2dba77c1964557a3d76177ec148160d88a5
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-12-05 21:17:39 -0500

    Add progress bar to topics list

--- quizapp/lib/topics/topic_item.dart
+++ quizapp/lib/topics/topic_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizapp/services/models.dart';
+import 'package:quizapp/shared/progress_bar.dart';
 
 class TopicItem extends StatelessWidget {
   final Topic topic;
@@ -44,7 +45,10 @@ class TopicItem extends StatelessWidget {
                             overflow: TextOverflow.fade,
                             softWrap: false,
                           )),
-                    )
+                    ),
+                    Flexible(
+                      child: TopicProgress(topic: topic),
+                    ),
                   ],
                 ))));
   }
