5a614d51752772154ba2e0e7ae248ab9f6536c21
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-12-10 10:59:44 -0500

    Add quiz state and basic scaffold

--- quizapp/lib/quiz/quiz.dart
+++ quizapp/lib/quiz/quiz.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:quizapp/quiz/quiz_state.dart';
+import 'package:quizapp/services/firestore.dart';
+import 'package:quizapp/services/models.dart';
+import 'package:quizapp/shared/progress_bar.dart';
+
+import '../shared/loading.dart';
 
 class QuizScreen extends StatelessWidget {
-  const QuizScreen({Key? key}) : super(key: key);
+  const QuizScreen({super.key, required this.quizId});
+  final String quizId;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ChangeNotifierProvider(
+        create: (_) => QuizState(),
+        child: FutureBuilder<Quiz>(
+          future: FirestoreService().getQuiz(quizId),
+          builder: (context, snapshot) {
+            var state = Provider.of<QuizState>(context);
+
+            if (!snapshot.hasData || snapshot.hasError) {
+              return const Loader();
+            } else {
+              var quiz = snapshot.data!;
+
+              return Scaffold(
+                appBar: AppBar(
+                  title: AnimatedProgressbar(value: state.progress),
+                  leading: IconButton(
+                    icon: const Icon(FontAwesomeIcons.xmark),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ),
+              );
+            }
+          },
+        ));
   }
 }
